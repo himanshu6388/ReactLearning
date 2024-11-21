16f6496e4056186aa0d73f04776d5d705c1c3595
himanshu6388 <himanshu219181@gmail.com>
2024-11-21 18:29:03 +0530

    updated

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <h1>Blog App with AppWrite.</h1>
+      <h1 className="bg-green-600 text-white p-2 text-center font-mono">Blog App with AppWrite.</h1>
     </>
   )
 }
