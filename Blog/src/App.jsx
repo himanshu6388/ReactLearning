0d4dcd86defa7fe6e162f6ba492f156f7387737b
himanshu6388 <himanshu219181@gmail.com>
2025-01-07 22:51:27 +0530

    updated

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -2,6 +2,7 @@
 
 function App() {
   
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
       <h1 className="bg-green-600 text-white p-2 text-center font-mono">Blog App with AppWrite.</h1>
